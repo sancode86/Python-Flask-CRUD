c3a0038996d49ed46b957090c84f76a4e6112531
Santiago Suarez <santiagosuarez86@gmail.com>
2022-01-24 15:39:54 -0300

    post

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 app = Flask(__name__);
@@ -30,6 +30,25 @@ def crear():
 @app.route('/editar')
 def editar():  
     return render_template('personas/editar.html')        
+
+@app.route('/guardar', methods=['POST'])
+def guardar(): 
+
+    _nombre=request.form['nombre']
+    _email=request.form['email']
+    _foto=request.files['foto']
+
+    sql = "INSERT INTO `personas` (`id`, `nombre`, `email`, `imagen`) VALUES (NULL, %s, %s, %s);"
+    
+    datos=(_nombre, _email, _foto.filename)
+
+    conn = mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit() 
+    return render_template('personas/index.html')        
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)    
