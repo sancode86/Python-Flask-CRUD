4ebd46c67b59b69a3054ff44490657cc98d42d2b
Santiago Suarez <santiagosuarez86@gmail.com>
2022-01-24 17:21:31 -0300

    varios

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import render_template, request,redirect
 from flaskext.mysql import MySQL
+from flask import send_from_directory
 from datetime import datetime
+import os
 
 app = Flask(__name__);
 
@@ -11,6 +13,13 @@ app.config['MYSQL_DATABASE_USER']= 'root'
 app.config['MYSQL_DATABASE_PASSWORD']= ''
 app.config['MYSQL_DATABASE_DB']= 'phytoncrud'
 mysql.init_app(app)
+
+CARPETA = os.path.join('uploads')
+app.config['CARPETA']=CARPETA
+
+@app.route('/uploads/nombreFoto')
+def uploads(nombreFoto):
+    return send_from_directory(app.config['CARPETA'], nombreFoto)
 
 @app.route('/')
 def index():
@@ -28,6 +37,11 @@ def index():
 def borrar(id):  
     conn = mysql.connect()
     cursor= conn.cursor()
+
+    cursor.execute("SELECT foto FROM personas WHERE id=%s", id)
+    fila=cursor.fetchall()
+    os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
+    
     cursor.execute("DELETE FROM personas WHERE id=%s", (id))
     conn.commit()
     return redirect('/')
@@ -60,6 +74,21 @@ def actualizar():
 
     conn = mysql.connect()
     cursor= conn.cursor()
+    
+    now = datetime.now()
+    tiempo = now.strftime("%Y%H%M%S")
+    
+    if _foto.filename != '':
+        nuevoNombreFoto = tiempo+_foto.filename
+        _foto.save("uploads/" + nuevoNombreFoto)
+
+        cursor.execute("SELECT foto FROM personas WHERE id=%s", id)
+        fila=cursor.fetchall()
+
+        os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
+
+        cursor.execute("UPDATE personas SET foto%s WHERE id%s",(nuevoNombreFoto, id))
+        conn.commit() 
     cursor.execute(sql, datos)
     conn.commit() 
 
